7741bc0fbb9df5f2b58ce1895c06dfc37dd81ce5
Brenorml <breno.lucena@gmail.com>
2022-07-13 15:20:59 +0100

    Solução do terceiro desafio

--- Desafios/desafio3.ts
+++ Desafios/desafio3.ts
@@ -1,37 +1,46 @@
-"use strict";
 // O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+
 let botaoAtualizar = document.getElementById('atualizar-saldo');
-let botaoLimpar = document.getElementById('limpar-saldo');
-let soma = document.getElementById('soma');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma') as HTMLInputElement;
 let campoSaldo = document.getElementById('campo-saldo');
+
 let saldoAtual = 0;
-function somarAoSaldo(soma) {
+
+
+function somarAoSaldo(soma: number) {
     if (campoSaldo) {
         saldoAtual += soma;
         campoSaldo.innerHTML = saldoAtual.toString();
         limparCampoSoma();
     }
 }
+
 function limparCampoSoma() {
     soma.value = "";
 }
+
 function limparSaldo() {
     if (campoSaldo) {
         saldoAtual = 0;
         campoSaldo.innerHTML = saldoAtual.toString();
     }
 }
+
 if (botaoAtualizar) {
     botaoAtualizar.addEventListener('click', () => {
         somarAoSaldo(Number(soma.value));
     });
 }
+
 botaoLimpar.addEventListener('click', () => {
     limparSaldo();
 });
+
 /**
     <h4>Valor a ser adicionado: <input id="soma"> </h4>
     <button id="atualizar-saldo">Atualizar saldo</button>
     <button id="limpar-saldo">Limpar seu saldo</button>
     <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
  */
+
